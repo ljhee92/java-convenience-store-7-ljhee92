70b638a17541f8408f46dddf1e8d84d4adcb6088
ljhee92 <weilainib7@gmail.com>
2024-11-08 17:13:37 +0900

    feat: 추가 구매 여부 관련 입력, 출력 기능 구현
    사용자 입력 - 추가 구매 여부 안내 답변 기능 구현
    - 입력 받기
    - 답변 형식 검증
    
    출력 - 추가 구매 여부를 묻는 메시지 출력 기능 구현

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,56 +1,60 @@
 package store.controller;
 
-import store.dto.ProductDTO;
-import store.domain.Order;
+import store.domain.Orders;
 import store.domain.Products;
+import store.domain.Promotions;
+import store.domain.Receipt;
+import store.dto.ProductDTO;
+import store.service.OrderService;
 import store.service.ProductService;
+import store.service.PromotionService;
+import store.util.RetryHandler;
 import store.view.InputView;
 import store.view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 public class StoreController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final RetryHandler retryHandler;
     private final ProductService productService;
+    private final OrderService orderService;
+    private final PromotionService promotionService;
 
-    public StoreController(InputView inputView, OutputView outputView, ProductService productService) {
+    public StoreController(InputView inputView, OutputView outputView, RetryHandler retryHandler,
+                           ProductService productService, OrderService orderService, PromotionService promotionService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.retryHandler = retryHandler;
         this.productService = productService;
+        this.orderService = orderService;
+        this.promotionService = promotionService;
     }
 
     public void run() {
-        List<ProductDTO> productsDTO = productService.searchAllProducts();
+        Products products = productService.getAllProducts();
+        Promotions promotions = promotionService.getAllPromotions();
+
+        do {
+            order(products, promotions);
+        } while (retryHandler.repeat(() -> inputView.requestReOrder().equals("Y")));
+    }
+
+    private void order(Products products, Promotions promotions) {
+        List<ProductDTO> productsDTO = productService.getAllProductsDTO(products);
         outputView.displayWelcomeAndProducts(productsDTO);
 
-        Products products = productService.getAllProducts();
-        List<Order> orders = repeat(() -> takeOrder(products));
+        Orders orders = retryHandler.repeat(() -> takeOrders(products));
+        orders.processOrder(promotions);
+
+        Receipt receipt = new Receipt(orders);
+        outputView.displayReceipt(receipt.toReceiptDTO());
     }
 
-    private List<Order> takeOrder(Products products) {
-        outputView.requestOrder();
+    private Orders takeOrders(Products products) {
         List<Map<String, String>> inputOrders = inputView.requestOrder();
-        List<Order> orders = new ArrayList<>(inputOrders.size());
-
-        inputOrders.forEach(inputOrder -> {
-            for (String name : inputOrder.keySet()) {
-                Order order = new Order(products, name, Integer.parseInt(inputOrder.get(name)));
-                orders.add(order);
-            }
-        });
-        return orders;
-    }
-
-    private <T> T repeat(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            outputView.displayErrorMessage(e.getMessage());
-            return repeat(supplier);
-        }
+        return new Orders(orderService.takeOrders(inputOrders, products));
     }
 }
--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,5 +1,6 @@
 package store.view;
 
+import store.exception.InvalidInputException;
 import store.exception.ProductFormatException;
 import store.util.ErrorMessage;
 import store.util.Parser;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 
 public class InputView extends UserInputReader {
     public List<Map<String, String>> requestOrder() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         String inputOrder = inputMessage();
         validateOrderFormat(inputOrder);
         return parseOrder(inputOrder);
@@ -21,7 +23,7 @@ public class InputView extends UserInputReader {
             throw new ProductFormatException(ErrorMessage.PRODUCT_FORMAT);
         }
 
-        if (!inputOrder.matches("^(\\[[가-힣]+-[0-9]+])*,*(\\[[가-힣]+-[0-9]+])$")) {
+        if (!inputOrder.matches("^(\\[[가-힣]+-[0-9]+])(,(\\[[가-힣]+-[0-9]+]))*$")) {
             throw new ProductFormatException(ErrorMessage.PRODUCT_FORMAT);
         }
     }
@@ -35,5 +37,22 @@ public class InputView extends UserInputReader {
                         order -> Parser.splitByHyphen(order).getLast()
                 ));
         return List.of(parsedOrder);
+    }
+
+    public String requestReOrder() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        String inputReOrder = inputMessage();
+        validateYesOrNoFormat(inputReOrder);
+        return inputReOrder;
+    }
+
+    private void validateYesOrNoFormat(String inputYesOrNo) {
+        if (inputYesOrNo.isBlank()) {
+            throw new InvalidInputException(ErrorMessage.INVALID_INPUT);
+        }
+
+        if (!"N".equals(inputYesOrNo) && !"Y".equals(inputYesOrNo)) {
+            throw new InvalidInputException(ErrorMessage.INVALID_INPUT);
+        }
     }
 }
