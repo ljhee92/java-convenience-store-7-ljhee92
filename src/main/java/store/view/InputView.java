32f1534508c7d2d5e23d721d4d763d05935d3bf4
ljhee92 <weilainib7@gmail.com>
2024-11-09 09:55:39 +0900

    style: import 정리

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,8 +7,6 @@ import store.util.UserInputReader;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class InputView extends UserInputReader {
     public List<List<String>> requestOrder() {
